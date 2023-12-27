32db5e5d29fa3fecc51afaf6d98fa8d6c0d42456
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2023-12-27 17:26:53 -0300

    Sorted reverse DTODadosEpisodio

--- src/main/java/br/com/yanvelasco/screemmatch/principal/Principal.java
+++ src/main/java/br/com/yanvelasco/screemmatch/principal/Principal.java
@@ -1,9 +1,12 @@
 package br.com.yanvelasco.screemmatch.principal;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+import br.com.yanvelasco.screemmatch.model.DTODadosEpisodio;
 import br.com.yanvelasco.screemmatch.model.DTODadosSerie;
 import br.com.yanvelasco.screemmatch.model.DTODadosTemporadas;
 import br.com.yanvelasco.screemmatch.service.ConsumoApi;
@@ -24,22 +27,33 @@ public class Principal {
         DTODadosSerie dtoDadosSerie = conversor.obterDados(json, DTODadosSerie.class);
         System.out.println(dtoDadosSerie);
 
-        List<DTODadosTemporadas>temporadas = new ArrayList<>();
+        List<DTODadosTemporadas> temporadas = new ArrayList<>();
 
-		for (int i = 1; i < dtoDadosSerie.totalDeTemporadas(); i++) {
+        for (int i = 1; i < dtoDadosSerie.totalDeTemporadas(); i++) {
             json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
-			DTODadosTemporadas dtoDadosTemporadas = conversor.obterDados(json, DTODadosTemporadas.class);
-			temporadas.add(dtoDadosTemporadas);
-		}
-		temporadas.forEach(System.out::println);
+            DTODadosTemporadas dtoDadosTemporadas = conversor.obterDados(json, DTODadosTemporadas.class);
+            temporadas.add(dtoDadosTemporadas);
+        }
+        temporadas.forEach(System.out::println);
 
         // for (DTODadosTemporadas temporada : temporadas) {
-        //     System.out.println("Temporada " + temporada.numeroTemporadas());
-        //     List<DTODadosEpisodio> episodiosTemporada = temporada.episodios();
-        //     for (DTODadosEpisodio episodio : episodiosTemporada) {
-        //         System.out.println(episodio.titulo());
-        //     }
+        // System.out.println("Temporada " + temporada.numeroTemporadas());
+        // List<DTODadosEpisodio> episodiosTemporada = temporada.episodios();
+        // for (DTODadosEpisodio episodio : episodiosTemporada) {
+        // System.out.println(episodio.titulo());
+        // }
         // }
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DTODadosEpisodio> dtoDadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        dtoDadosEpisodios.stream()
+        .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+        .sorted(Comparator.comparing(DTODadosEpisodio::avaliacao)
+        .reversed())
+        .limit(5)
+        .forEach(System.out::println);
     }
 }
